56d2ebd77d70bbb1476e7821a8b8af840f6e30d4
Iliya Glazunov <iliyasalve@gmail.com>
2024-10-12 18:18:31 +0200

    Modify init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 window.onload = () => {
     isDarkTheme = localStorage.getItem('theme') === 'dark'; 
     updateTheme();
-    console.log("Тема при загрузке:", isDarkTheme ? "темная" : "светлая"); 
     initCookieModal();
 };
